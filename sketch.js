3d0ef3e6852bcd32eac51abdc6eb88ca2e889f8d
DDDDDYLAN5050 <55461461+DDDDDYLAN5050@users.noreply.github.com>
2019-11-13 01:56:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,47 @@
-function preload(){
-  // put preload code here
+var myMap;
+var canvas;
+var mappa = new Mappa("Leaflet");
+var poliLat = 45.5069578;
+var poliLon = 9.1639516;
+var currentLat, currentLon;
+var currentPosition;
+var distance;
+var options = {
+  lat: poliLat,
+  lng: poliLon,
+  zoom: 15,
+  style:"http://tile.stamen.com/toner/{z}/{x}/{y}.png"
+};
+
+function preload() {
+  currentPosition = getCurrentPosition();
 }
 
 function setup() {
-  // put setup code here
+  console.log(currentPosition);
+  currentLat = currentPosition.latitude;
+  currentLon = currentPosition.longitude;
+  distance = calcGeoDistance(currentLat, currentLon, poliLat, poliLon, "km");
+  canvas = createCanvas(windowWidth, windowHeight);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+  colorMode(HSB);
 }
 
 function draw() {
-  // put drawing code here
+  clear();
+  var polimi = myMap.latLngToPixel(poliLat, poliLon);
+  var yourLocation = myMap.latLngToPixel(currentLat,currentLon);
+  strokeWeight(5);
+  stroke(frameCount*3%360,50,80);
+  textSize(30);
+  textStyle(BOLD);
+  textFont('Helvetica');
+  textAlign(CENTER);
+  line(yourLocation.x,yourLocation.y,polimi.x, polimi.y);
+  fill('black');
+  text('The distance between you and B10 = '+int(1000*distance)+'m',width/2,60);
+  text(int(distance*1000)/1000 +'km',(yourLocation.x+polimi.x)/2,(yourLocation.y+polimi.y)/2);
+  ellipse(yourLocation.x,yourLocation.y,10);
+  ellipse(polimi.x, polimi.y, 10);
 }
